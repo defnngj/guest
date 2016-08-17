4321706b6f274ba673879816c991fdd82cc443ea
defnngj <defnngj@gmail.com>
2016-08-17 18:23:04 +0800

    add interface test case

--- sign/function_tests/interface_test.py
+++ sign/function_tests/interface_test.py
@@ -1,17 +1,19 @@
+#coding=utf-8
 import unittest
 import requests
 
 
-class MyTest(unittest.TestCase):
+class AddEventTest(unittest.TestCase):
 
+    def setUp(self):
+        self.base_url = "http://127.0.0.1:8000/sign/add_event/"
 
-    def test_login(self):
-        payload = {'username': 'admin', 'password': 'admin123456'}
-        r = requests.post("http://127.0.0.1:8000/login_action/", data=payload)
-        print(r.status_code)
-        print(r.text)
-
-
+    def test_add_event_all_null(self):
+        ''' 所有参数为空 '''
+        r = requests.post(self.base_url)
+        result = r.json()
+        self.assertEqual(result['ststus'], 10021)
+        self.assertEqual(result['message'], 'parameter error')
 
 if __name__ == '__main__':
     unittest.main()
