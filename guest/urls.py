e212ba3848921552e86fab27b9f80a3cb995e8e4
fnngj <fnngj@126.com>
2016-09-27 23:36:36 +0800

    Modify the interface name

--- guest/urls.py
+++ guest/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     url(r'^sreach_phone/$', views.sreach_phone),
     url(r'^sign_index/(?P<event_id>[0-9]+)/$', views.sign_index),
     url(r'^sign_index_action/(?P<event_id>[0-9]+)/$', views.sign_index_action),
-    url(r'^sign/', include('sign.urls', namespace="sign")),
+    url(r'^api/', include('sign.urls', namespace="sign")),
 
 ]
