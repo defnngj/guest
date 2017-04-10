60009dec42217e06d5c6b3babe88c259bfa9a45e
defnngj <defnngj@gmail.com>
2017-04-10 16:39:04 +0800

    database config.

--- guest/settings.py
+++ guest/settings.py
@@ -76,26 +76,11 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'NAME': 'guest',
-        'USER': 'root',
-        'PASSWORD': '198876',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            },
-        }
-}
-
-'''
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
